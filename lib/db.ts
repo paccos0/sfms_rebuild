b737d1ecddb6279eefa46e4537671f9fc35e96e3
paccos0 <ggamk24@gmail.com>
2026-04-22 02:43:41 +0200

    env

--- lib/db.ts
+++ lib/db.ts
@@ -1,28 +1,9 @@
-import mysql, { Pool } from "mysql2/promise";
+import mysql from "mysql2/promise";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __mysql_pool__: Pool | undefined;
-}
-
-const pool =
-  global.__mysql_pool__ ||
-  mysql.createPool({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "regohoff",
-    database: "sfms",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    namedPlaceholders: true,
-    decimalNumbers: true,
-    connectTimeout: 10000,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  global.__mysql_pool__ = pool;
-}
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 export default pool;
